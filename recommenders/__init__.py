8b5f565b2840e360f398b8503f3a6843c6392ca2
bjorn-uix <106654277+bjorn-uix@users.noreply.github.com>
2023-09-29 04:18:11 +0530

    task: Task: Coding Question

--- recommenders/__init__.py
+++ recommenders/__init__.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-print(os.getcwd())
+print(os.getcwd());
 
 data_ratings = pd.read_csv('data_files/ratings.csv')
 data_movies = pd.read_csv('data_files/movies.csv')
